2f506929c81790feeda31f860168b2ad949870f0
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-07-26 17:56:56 +0300

    correct error in Map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,13 +2,16 @@ const Map = () => {
     return (
         <div className="mt-5">
             <div className="main-container">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3412.6422805551056!2d29.905658123766862!3d31
+            <iframe 
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3412.6422805551056!2d29.905658123766862!3d31
             .20293046275577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!
             1m2!1s0x14f5c38f6445bf23%3A0x703089933f8b7455!2z2YXYrdi32Ycg2KfZhNix2YXZhCDYp9mE2KfYs9mD2YbYr9ix2YrZhw!5e0!3m2!1sar!2seg!4v1721934569470!5m2!1sar!2seg"
              width="600" height="450"
-              style="border:0;
-              " allowfullscreen="" loading="lazy"
-               referrerpolicy="no-referrer-when-downgrade">
+              style={{border: "0"}}
+               allowFullScreen
+                loading="lazy"
+               referrerPolicy="no-referrer-when-downgrade" 
+               >
             </iframe>
             </div>
         </div>
